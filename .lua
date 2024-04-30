78ea650e81bf44f4b433f149d2f3da37db1cf950
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-05-01 07:10:58 +0900

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,50 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
+local wndw = lib:Window("VIP Turtle Hub V4")
+local T1 = wndw:Tab("Main")
+local T2 = wndw:Tab("Hatch")
+
+local workspace = game:GetService("Workspace")
+local var = {
+  click = false,
+  zone = {},
+  szone = "Clown Island",
+  farm = false,
+  ezone = "Clown Island",
+  egg = false
+}
+
+lib:AddTable(workspace["Server"]["Enemies"]["World"],var.zone)
+
+T1:Dropdown("Choose island to farm",var.zone,function(value)
+    var.szone = value
+end)
+
+T1:Toggle("Auto click",false,function(value)
+    var.click = value
+    while wait() do
+      if var.click == false then break end
+      game:GetService("ReplicatedStorage")["Bridge"]:FireServer("Attack","Click",{["Type"] = "Invasion"})
+    end
+end)
+
+T1:Toggle("Auto kill",false,function(value)
+    var.farm = value
+    while wait() do
+      if var.farm == false then break end
+      lib:children(workspace["Server"]["Enemies"]["World"][var.szone],function(v)
+          game:GetService("ReplicatedStorage")["Bridge"]:FireServer("Attack","Click",{["Enemy"] = v,["Type"] = "World"})
+      end)
+    end
+end)
+
+T2:Dropdown("Choose island to open a egg",var.zone,function(value)
+    var.ezone = value
+end)
+
+T2:Toggle("Auto open",false,function(value)
+    var.egg = value
+    while wait() do
+      if var.egg == false then break end
+      game:GetService("ReplicatedStorage")["Bridge"]:FireServer("Stars","Roll",{["Map"] = var.ezone,["Type"] = "Open"})
+    end
+end)
